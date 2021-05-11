97e17345b882b9437715240f1361cc186fd029d0
ZEHLAND <80832916+ZEHLAND@users.noreply.github.com>
2021-05-11 23:10:29 +0300

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,31 @@
+﻿#include<stdlib.h>
+#include <iostream>
+#include<cstdio>    ""
+using namespace std;
+class Insan {
+    public://eklenmezse private olarak tanımlanıyor tüm nesneleri
+    char ad[50];
+    char* soyad;
+    int yas;
+    double boy;
+    void kos();
+    void dur() {
+        cout << "Durdum." << endl;
+    }
+};
+void Insan::kos() {
+    cout << "Kosuyorum." << endl;
+}
+int main()
+{
+    Insan a;
+    strcpy_s(a.ad, "mehmet");
+    a.soyad = (char*)"agca";
+    a.yas = 18;
+    a.boy = 1.80;
+    cout << a.ad << "kos." << endl;
+    a.kos();
+
+    return 0;
+}
+
